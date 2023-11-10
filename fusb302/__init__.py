4a9b166f265979ccd11940562ebd40dd2df65f55
Stefan Groothuis <info@technlgyst.com>
2023-11-10 12:21:05 +0100

    Initial copy of working code from inside esphome

---
+++ fusb302/__init__.py
@@ -0,0 +1,54 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.const import (
+    CONF_ID,
+    CONF_INTERRUPT_PIN,
+    #CONF_ENABLE_PIN,
+)
+
+CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE = 'maximum_supply_voltage'
+
+fusb302_ns = cg.esphome_ns.namespace('fusb302')
+FUSB302 = fusb302_ns.class_('FUSB302', cg.Component)
+
+MaximumSupplyVoltage = fusb302_ns.enum("MAXIMUM_SUPPLY_VOLTAGE")
+
+MAXIMUM_SUPPLY_VOLTAGE_OPTIONS = {
+    "5v": MaximumSupplyVoltage.PD5V,
+    "9v": MaximumSupplyVoltage.PD9V,
+    "12v": MaximumSupplyVoltage.PD12V,
+    "15v": MaximumSupplyVoltage.PD15V,
+    "20v": MaximumSupplyVoltage.PD20V,
+}
+
+#def validate_rx_pin(value):
+#    value = pins.internal_gpio_input_pin_schema(value)
+#    if CORE.is_esp8266 and value[CONF_NUMBER] >= 16:
+#        raise cv.Invalid("Pins GPIO16 and GPIO17 cannot be used as RX pins on ESP8266.")
+#    return value
+
+
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(FUSB302),
+    cv.Required(CONF_INTERRUPT_PIN): cv.All(pins.internal_gpio_input_pin_schema),
+    cv.Optional(CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE, default="5v"): cv.enum(MAXIMUM_SUPPLY_VOLTAGE_OPTIONS, lower=True),
+#    cv.Required(CONF_ENABLE_PIN): pins.internal_gpio_output_pin_schema,
+#    cv.Optional(CONF_ID, default=usb_powerdelivery): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    if CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE in config:
+        max_supply_voltage = config[CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE]
+        cg.add(var.set_maximum_supply_voltage(max_supply_voltage))
+    
+    if CONF_INTERRUPT_PIN in config:
+        interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
+        cg.add(var.set_interrupt_pin(interrupt_pin))
+
+
---
+++ fusb302/fusb302.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "esphome/core/gpio.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace fusb302 {
+
+enum MaximumSupplyVoltage {PD5V, PD9V, PD12V, PD15V, PD20V};
+
+class FUSB302 : public Component {
+  public:
+    void set_maximum_supply_voltage(MaximumSupplyVoltage maximum_supply_voltage) { maximum_supply_voltage_ = maximum_supply_voltage; }
+    //void set_interrupt_pin(uint8_t interruptPinGPIO) { interruptPin = interruptPinGPIO; }
+    void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
+
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+
+  protected:
+    MaximumSupplyVoltage maximum_supply_voltage_{PD5V};
+    InternalGPIOPin *interrupt_pin_;
+};
+}  // namespace fusb302
+}  // namespace esphome
